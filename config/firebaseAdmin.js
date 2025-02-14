017528181a44a848e42f0d889fa4b4db88764c4f
Erin Ballinger <erballin@iu.edu>
2025-02-14 10:45:56 -0500

    alt fix

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -9,7 +9,8 @@ console.log(process.env.FIREBASE_PRIVATE_KEY);
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    // private_key: process.env.FIREBASE_PRIVATE_KEY,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.split(String.raw`\n`).join("\n"),
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
   }),
   // databaseURL: "https://your-project-id.firebaseio.com", // Replace with your database URL if using Firestore
