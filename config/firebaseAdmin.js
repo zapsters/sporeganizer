8ed70d009725443d31f7a0a1f656ff815d18eea5
Erin Ballinger <erballin@iu.edu>
2025-02-14 09:46:13 -0500

    Env variables added

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,11 +1,15 @@
 const admin = require("firebase-admin");
 
 // Load Firebase credentials from a service account JSON file
-const credentials = require("../firebase-service-account.json");
+// const credentials = require("../firebase-service-account.json");
 
 // Initialize Firebase Admin
 admin.initializeApp({
-  credential: admin.credential.cert(credentials),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
   // databaseURL: "https://your-project-id.firebaseio.com", // Replace with your database URL if using Firestore
 });
 
