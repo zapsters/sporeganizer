81b990f4bc1a4091fa43aa364de6bc864b4556e0
Erin Ballinger <erballin@iu.edu>
2025-02-14 10:04:45 -0500

    json parse env var

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -7,7 +7,7 @@ const admin = require("firebase-admin");
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    private_key: JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
   }),
   // databaseURL: "https://your-project-id.firebaseio.com", // Replace with your database URL if using Firestore
