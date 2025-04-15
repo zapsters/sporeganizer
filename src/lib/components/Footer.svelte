091fd95ebbecd71a71dfc77aba752f2ff639c824
zapsters <erinballinger24@gmail.com>
2025-04-14 20:09:03 -0400

    Fixed grammer + removed leftover css style

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -9,7 +9,7 @@
 		>See more at <a target="_blank" href="https://erinballinger.com">erinballinger.com</a></span
 	>
 	<span style="font-size: smaller; margin: -3px 0px 5px 0px !important;"
-		>What to support me? <i
+		>Want to support me? <i
 			style="font-size: 28px; display: inline-block; transform: translate(-5px, 6px); width: 0px; height: 0px;"
 			class="pixelart-icons-font-coffee-alt primaryRecolor"
 		></i><a
