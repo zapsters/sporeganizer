89c696180a921d4da88fb638fba525cc458334d3
zapsters <erinballinger24@gmail.com>
2025-07-12 19:29:22 -0400

    + Updated UI Scale + Changed hero image + Small Style Adjustments + Updated Footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,14 +1,7 @@
 <footer>
-	<span>Made with ❤ by Erin Ballinger</span>
-	<span style="font-size: smaller; font-weight: 100 !important;"
-		>Banner image and Mushroom artwork was designed by <a href="https://www.instagram.com/mushollow"
-			>Mushollow</a
-		></span
+	<span>Made with ❤ by <a target="_blank" href="https://erinballinger.com">Erin Ballinger</a></span
 	>
-	<span style="font-size: smaller; margin-top: 0px;"
-		>See more at <a target="_blank" href="https://erinballinger.com">erinballinger.com</a></span
-	>
-	<span style="font-size: smaller; margin: -3px 0px 5px 0px !important;"
+	<!-- <span style="font-size: smaller; margin: -3px 0px 5px 0px !important;"
 		>Want to support me? <i
 			style="font-size: 28px; display: inline-block; transform: translate(-5px, 6px); width: 0px; height: 0px;"
 			class="pixelart-icons-font-coffee-alt primaryRecolor"
@@ -17,5 +10,5 @@
 			href="https://buymeacoffee.com/erinballinger"
 			style="padding-left: 25px;">Buy me a coffee!</a
 		></span
-	>
+	> -->
 </footer>
